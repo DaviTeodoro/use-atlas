79e69e847712bd783f7a73773e1a8fbcf03d91b5
Davi Teodoro <davi.a.teodoro@gmail.com>
2022-01-30 13:42:26 -0300

    adds logict to update on data change

--- src/hooks/useChoropleth.js
+++ src/hooks/useChoropleth.js
@@ -1,10 +1,82 @@
 import { useEffect, useReducer, useState } from 'react';
 import useLayer from './useLayer';
-import { scaleThreshold } from 'd3-scale';
+import d3Scale from 'd3-scale';
 import { color } from 'd3-color';
 
+export default function useChoropleth(config, data = []) {
+  const [{ setData: _setData, setConfig }, layer] = useLayer(
+    {
+      ...config,
+      getFillColor: (object) => object.fillColor
+    },
+    data
+  );
+
+  const [{ indicator, domain, colorRange }, dispatch] = useReducer(reducer, {
+    indicator: config.indicator,
+    domain: config.domain,
+    colorRange: config.colorRange
+  });
+
+  const [isSet, setIsSet] = useState(false);
+
+  useEffect(() => {
+    if (layer?.data && !isSet) {
+      setData(layer.data);
+      setIsSet(true);
+    }
+  }, [layer]);
+
+  useEffect(() => {
+    if (layer?.data) {
+      setData(layer.data);
+    }
+  }, [indicator, domain, colorRange]);
+
+  function setDomain(domain) {
+    dispatch({ type: 'SET_DOMAIN', payload: domain });
+  }
+
+  function setIndicator(indicator) {
+    dispatch({ type: 'SET_INDICATOR', payload: indicator });
+  }
+
+  function setColorRange(colorRange) {
+    dispatch({ type: 'SET_COLOR_RANGE', payload: colorRange });
+  }
+
+  function setData(data) {
+    if (data) {
+      _setData(generateChoropleth(data, indicator, domain, colorRange));
+    }
+  }
+
+  return [
+    { setData, setIndicator, setDomain, setColorRange, setConfig },
+    layer
+  ];
+}
+
+function generateChoropleth(data, indicator, domain, colorRange) {
+  const colorScale = d3Scale['scaleThreshold']()
+    .domain(domain)
+    .range(colorRange);
+
+  const isArray = data instanceof Array;
+  const features = isArray ? data : data.features;
+
+  return features.map((d) => {
+    if (!d.properties[indicator]) {
+      throw new Error(`Indicator "${indicator}" not found.`);
+    }
+    const rgbColor = color(colorScale(d.properties[indicator]));
+
+    d.fillColor = [rgbColor.r, rgbColor.g, rgbColor.b, 160];
+    return d;
+  });
+}
+
 function reducer(state, { type, payload }) {
-  // console.log(type, payload)
   switch (type) {
     case 'SET_INDICATOR': {
       return {
@@ -28,64 +100,4 @@ function reducer(state, { type, payload }) {
       throw new Error(`Unhandled action type: ${type}`);
     }
   }
-}
-
-function generateChoropleth(data, indicator, domain, colorRange) {
-  const colorScale = scaleThreshold().domain(domain).range(colorRange);
-  return data.map((d) => {
-    if (!d.properties[indicator]) {
-      throw new Error(`Indicator "${indicator}" not found.`);
-    }
-    const rgbColor = color(colorScale(d.properties[indicator]));
-    d.fillColor = [rgbColor.r, rgbColor.g, rgbColor.b, 160];
-    return d;
-  });
-}
-
-export default function useChoropleth(config, data) {
-  const [{ setData, setConfig }, layer] = useLayer(
-    {
-      ...config,
-      getFillColor: (object) => object.fillColor
-    },
-    data
-  );
-
-  const [{ indicator, domain, colorRange }, dispatch] = useReducer(reducer, {
-    indicator: config.indicator,
-    domain: config.domain,
-    colorRange: config.colorRange
-  });
-
-  const [isSet, setIsSet] = useState(false);
-
-  useEffect(() => {
-    if (layer?.data && !isSet) {
-      setData(generateChoropleth(layer.data, indicator, domain, colorRange));
-      setIsSet(true);
-    }
-  }, [layer]);
-
-  useEffect(() => {
-    if (layer?.data) {
-      setData(generateChoropleth(layer.data, indicator, domain, colorRange));
-    }
-  }, [indicator, domain, colorRange]);
-
-  const setDomain = (domain) => {
-    dispatch({ type: 'SET_DOMAIN', payload: domain });
-  };
-
-  const setIndicator = (indicator) => {
-    dispatch({ type: 'SET_INDICATOR', payload: indicator });
-  };
-
-  const setColorRange = (colorRange) => {
-    dispatch({ type: 'SET_COLOR_RANGE', payload: colorRange });
-  };
-
-  return [
-    { setData, setIndicator, setDomain, setColorRange, setConfig },
-    layer
-  ];
 }
